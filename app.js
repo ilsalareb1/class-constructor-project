975bedfa6cc71b441967a2ed95e5b57f0290d2c7
ilsalareb1 <50841067+ilsalareb1@users.noreply.github.com>
2019-10-08 11:27:15 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+///one class to create the object person, album, movie ***Model
+//take arguments 
+class Movies{
+    constructor(title, director, yearReleased){
+        this.tile = title;
+        this.director = director;
+        this.yearReleased = yearReleased;
+    }
+}
+
+
+///***View */class for the UI which contains methods
+
+//creates/insert the HTML for the UI display 
+//remove object
+
+
+
+///controller combines ui and model
+
+
+///event handler with function
+//get values
+
+
+///instantiate new class *makes new album or movie
+
+
+///instantiate new UI const ui = new UI ()
+
+
+///call relevent methods
+
+
+///separate event handler for removing/deleting 
